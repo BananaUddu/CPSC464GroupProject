9177ed2de62428cef4be82d66d7ecb649eaa9c4b
Matthew Kernan <43825492+mpk1005@users.noreply.github.com>
2019-09-30 15:08:23 -0400

    Add files via upload

---
+++ Zero_App/src/Device.java
@@ -0,0 +1,13 @@
+
+public class Device {
+	static void Device() {
+		String logicalName = null;
+		String physicalName = null;
+		boolean status = false;
+		int speed = 0;
+		int numProcesses = 0;
+		
+	}
+	
+
+}
---
+++ Zero_App/src/DeviceList.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class DeviceList extends Device {
+	static void DeviceList() {
+		LinkedList<Device> DeviceList1 = new LinkedList<Device>();
+	}
+	static void RemoveDeviceList(){
+		
+	}
+	static void AddDevice() {
+		
+	}
+	static void printDevices() {
+	
+	}
+	static void addProcess() {
+			
+	}
+	static void decrementDevices() {
+		
+	}
+	static void checkStatus() {
+		
+	}
+
+}
