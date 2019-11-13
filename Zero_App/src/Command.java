a1426d3b45c2f2d49b149f01c107f974ff0721c0
Edward Vance <32777268+Edvard31@users.noreply.github.com>
2019-11-12 22:27:06 -0500

    Update Command.java

--- Zero_App/src/Command.java
+++ Zero_App/src/Command.java
@@ -1,64 +1,46 @@
-public class CommandNode {
-  public String request;
-	public double length;
-	CommandNode nextCommand;
-	CommandNode classptr;
-	CommandNode currentptr;
-	
-	public CommandNode(String request, double length) //constructor
-	{
-		this.request = request;
-		this.length = length;
-	}
-	
-	public void print()
-	{
-		System.out.println("This Works: " + request + "and " + length);
-	}
-	
-	public String toString()
-	{
-		return request;
-	}
-}
+import java.util.LinkedList;
+
 public class Command {
-  public CommandNode beginNode;
-	
-	Command()
-	{
-		beginNode = null;
-		beginNode.classptr = null;
-		beginNode.currentptr = null;
-	}
-	
-	public void clearCommand()
-	{
-		CommandNode length = beginNode.size();
-		for (int i = 0; i < size; i++) {
-			length.remove(i);
-	}
-	
-	public void addEntry(String request, int length)
-	{
-		CommandNode newNode = new CommandNode(request, length);
-		newNode.nextCommand = beginNode;
-		beginNode = newNode;
-	}
-	
-	public int SendLength(int length)
-	{
-		return length;
-	}
-	
-	public void print()
-	{
-		CommandNode commands = beginNode;
-		while(commands != null)
+	LinkedList<String> CommandNode;
+	public String request;
+	public double length;
+	public Command nextCommand;
+	Command classptr;
+	Command currentptr;
+	Command head;
+			
+		Command()
 		{
-			commands.print();
-			System.out.println("Next Node works: " + commands.nextCommand);
-			commands = commands.nextCommand;
-			System.out.println();
+			classptr = null;
+			currentptr = null;
+		}
+			
+		public void clearCommand()
+		{
+			head = null;
+		}
+			
+		public void addEntry(String request, int length)
+		{
+			this.request = request;
+			this.length = length;
+		}
+			
+		public int SendLength(int length)
+		{
+			return length;
+		}
+			
+		public void print()
+		{
+			Command commands = head;
+			while(commands != null)
+			{
+				commands.print();
+				System.out.println("Next Node works: " + commands.nextCommand);
+				commands = commands.nextCommand;
+				System.out.println();
+			}
 		}
 	}
-}
+
